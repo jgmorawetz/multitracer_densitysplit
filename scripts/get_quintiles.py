a695a470232fe803f3825546d61405207f37bf00
jgmorawetz <james.morawetz@gmail.com>
2022-10-25 10:54:39 -0400

    Cross correlations for smaller volumes

--- scripts/get_quintiles.py
+++ scripts/get_quintiles.py
@@ -29,38 +29,33 @@ def get_data_positions(data_fn, split='z', los='z'):
 
 
 if __name__ == '__main__':
-    for split in ['z', 'r']: # split is whether redshift or real space
-        boxsize = 2000 # length of simulation cube
-        smooth_ds = 20 # smooth radius
-        nquantiles = 5 # number of percentiles to use
-        los = 'z' # line of sight direction
-        cellsize = 5 # size of the cells within the cube
-        
-        # run density split
-        data_dir = os.path.join('/home/jgmorawe/projects/rrg-wperciva/',
-                                'AbacusSummit/AbacusSummit_base_c000_ph000/',
-                                'halos/z0.575')
-        # uses cosmo = 0, phase = 0, redshift = 0.575 (may need to adjust)
-        data_fn = os.path.join(data_dir,
-                    'halos_base_c000_ph000_z0.575_nden3.2e-04.fits')
-        
-        # retrieves x,y,z data positions and counts number of objects
-        data_positions = get_data_positions(data_fn=data_fn, split=split, 
-                                            los=los)
-        ndata = len(data_positions)
-        
-        # initiates density split object
-        ds = DensitySplit(data_positions, boxsize)
-        
-        # generates random sampling positions and computes density
-        sampling_positions = np.random.uniform(0, boxsize, (5 * ndata, 3))
-        density = ds.get_density(smooth_radius=smooth_ds, cellsize=cellsize,
-                                 sampling_positions=sampling_positions)
-        
-        # retrieves the positions in each quantile
-        quantiles = ds.get_quantiles(nquantiles=5)
-        
-        # saves results
-        output_fn = os.path.join('/home/jgmorawe/results/quintiles',
-            f'quantiles_{split}split_base_c000_ph000_z0.575_nden3.2e-04.npy')
-        np.save(output_fn, quantiles)
+    split = 'r' # work in real space for now
+    boxsize = 500 # length of simulation cube
+    smooth_ds = 20 # smooth radius
+    nquantiles = 5 # number of percentiles to use
+    los = 'z' # line of sight direction
+    cellsize = 5 # size of the cells within the cube
+    
+    output_folder = '/home/jgmorawe/results/quintiles/real'
+    
+    sim_nums = np.arange(3000, 5000)
+    sim_path_start = '/home/jgmorawe/projects/rrg-wperciva/AbacusSummit/small'
+    
+    for sim_num in sim_nums:
+        sim_path = os.path.join(sim_path_start, 
+                                'AbacusSummit_small_c000_ph{}/halos/z0.575'.format(sim_num),
+                                'halos_small_c000_ph{}_z0.575_nden3.2e-04.fits'.format(sim_num))
+        if os.path.exists(sim_path):
+            data_positions = get_data_positions(sim_path, 'r', 'z')
+            ds = DensitySplit(data_positions, boxsize)
+            density = ds.get_density(smooth_radius=smooth_ds, cellsize=cellsize,
+                                     sampling_positions=data_positions)
+            quantiles = ds.get_quantiles(nquantiles=5)
+            # saves results
+            np.save(os.path.join(output_folder, 'sim_{}_ds1.npy'.format(sim_num)), quantiles[0])
+            np.save(os.path.join(output_folder, 'sim_{}_ds2.npy'.format(sim_num)), quantiles[1])
+            np.save(os.path.join(output_folder, 'sim_{}_ds3.npy'.format(sim_num)), quantiles[2])
+            np.save(os.path.join(output_folder, 'sim_{}_ds4.npy'.format(sim_num)), quantiles[3])
+            np.save(os.path.join(output_folder, 'sim_{}_ds5.npy'.format(sim_num)), quantiles[4])
+        else:
+            print('No data for {}'.format(sim_num))
