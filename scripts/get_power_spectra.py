54ad8ee188fd5ed766660bcf77fa2c25b3ff5904
jgmorawetz <james.morawetz@gmail.com>
2022-11-30 00:13:17 -0500

    Now using MPI

--- scripts/get_power_spectra.py
+++ scripts/get_power_spectra.py
@@ -9,7 +9,7 @@ import time
 import pickle
 import numpy as np
 from astropy.io import fits
-from pypower import CatalogFFTPower
+from pypower import CatalogFFTPower, mpi
 
 
 
@@ -35,6 +35,10 @@ if __name__ == '__main__':
     
     t0 = time.time()
     
+    # multiprocessing
+    mpicomm = mpi.COMM_WORLD
+    mpiroot = 0
+    
     # set the relevant parameters
     boxsize = 500
     smooth_ds = 20
@@ -42,114 +46,88 @@ if __name__ == '__main__':
     los = 'z'
     split = 'r'
     
+    # relevant file paths
     sim_folder_path = '/home/jgmorawe/projects/rrg-wperciva/AbacusSummit/small'
     quantile_path = '/home/jgmorawe/results/quantiles/quintiles/real'
     power_folder_path = '/home/jgmorawe/results/power_spectra/monopole/real'
     
+    # first retrieves all the simulation numbers without errors
+    valid_sim_nums = []
     for sim_num in range(3000, 5000):
-        
         sim_path = os.path.join(
             sim_folder_path,
             'AbacusSummit_small_c000_ph{}/halos/z0.575'.format(sim_num),
             'halos_small_c000_ph{}_z0.575_nden3.2e-04.fits'.format(sim_num))
-        
         if os.path.exists(sim_path):
-            # halo and quintile positions
-            halo_positions = get_data_positions(
-                data_fn=sim_path, split=split, los=los)
+            valid_sim_nums.append(sim_num)
+            
+    # sets bin edges for power
+    edges = np.linspace(0.01, 0.5, 451)
+    
+    # generates auto power spectra first
+    for sim_num in valid_sim_nums:
+        for quintile in ['1', '5']:
+            if mpicomm.rank == mpiroot:
+                ds_positions = np.load(
+                    os.path.join(quantile_path, 'sim{0}_ds{1}.npy'.format(sim_num, quintile)))
+            else:
+                ds_positions = None
+            auto = CatalogFFTPower(
+                data_positions1=ds_positions, edges=edges, ells=(0), los=los,
+                boxsize=boxsize, cellsize=cellsize, position_type='pos',
+                mpicomm=mpicomm, mpiroot=mpiroot)
+            pickle.dump(
+                auto, open(os.path.join(power_folder_path, 
+                'sim{0}_auto_ds{1}.pkl'.format(sim_num, quintile)), 'wb'))
+    print('Auto Power Spectra completed!')
+    
+    # generates cross power spectra with the halo positions next
+    for sim_num in valid_sim_nums:
+        for quintile in ['1', '5']:
+            if mpicomm.rank == mpiroot:
+                sim_path = os.path.join(
+                    sim_folder_path,
+                    'AbacusSummit_small_c000_ph{}/halos/z0.575'.format(sim_num),
+                    'halos_small_c000_ph{}_z0.575_nden3.2e-04.fits'.format(sim_num))
+                ds_positions = np.load(
+                    os.path.join(quantile_path, 'sim{0}_ds{1}.npy'.format(sim_num, quintile)))
+                halo_positions = get_data_positions(
+                    data_fn=sim_path, split=split, los=los)
+            else:
+                ds_positions = None
+                halo_positions = None
+            cross = CatalogFFTPower(
+                data_positions1=ds_positions, data_positions2=halo_positions,
+                edges=edges, ells=(0), los=los,
+                boxsize=boxsize, cellsize=cellsize, position_type='pos',
+                mpicomm=mpicomm, mpiroot=mpiroot)
+            pickle.dump(
+                cross, open(os.path.join(power_folder_path, 
+                'sim{0}_cross_ds{1}-halo.pkl'.format(sim_num, quintile)), 'wb'))
+    print('Cross-Halo Power Spectra completed!')
+    
+    # generates cross power spectra with each other finally
+    for sim_num in valid_sim_nums:
+        if mpicomm.rank == mpiroot:
             ds1_positions = np.load(
                 os.path.join(quantile_path, 'sim{}_ds1.npy'.format(sim_num)))
-            ds2_positions = np.load(
-                os.path.join(quantile_path, 'sim{}_ds2.npy'.format(sim_num)))
-            ds3_positions = np.load(
-                os.path.join(quantile_path, 'sim{}_ds3.npy'.format(sim_num)))
-            ds4_positions = np.load(
-                os.path.join(quantile_path, 'sim{}_ds4.npy'.format(sim_num)))
             ds5_positions = np.load(
                 os.path.join(quantile_path, 'sim{}_ds5.npy'.format(sim_num)))
-            
-            edges = np.arange(2*np.pi/boxsize, 
-                              2*np.pi/(boxsize/10)+2*np.pi/boxsize,
-                              2*np.pi/boxsize)
-            
-            # power spectrum of each quintile with halo positions
-            cross1 = CatalogFFTPower(
-                data_positions1=ds1_positions, data_positions2=halo_positions,
-                edges=edges, ells=(0), los=los, boxsize=boxsize, 
-                cellsize=cellsize, position_type='pos')
-            cross2 = CatalogFFTPower(
-                data_positions1=ds2_positions, data_positions2=halo_positions,
-                edges=edges, ells=(0), los=los, boxsize=boxsize, 
-                cellsize=cellsize, position_type='pos')
-            cross3 = CatalogFFTPower(
-                data_positions1=ds3_positions, data_positions2=halo_positions,
-                edges=edges, ells=(0), los=los, boxsize=boxsize, 
-                cellsize=cellsize, position_type='pos')
-            cross4 = CatalogFFTPower(
-                data_positions1=ds4_positions, data_positions2=halo_positions,
-                edges=edges, ells=(0), los=los, boxsize=boxsize, 
-                cellsize=cellsize, position_type='pos')
-            cross5 = CatalogFFTPower(
-                data_positions1=ds5_positions, data_positions2=halo_positions,
-                edges=edges, ells=(0), los=los, boxsize=boxsize, 
-                cellsize=cellsize, position_type='pos')
-            
-            # auto spectrum of each quintile with itself (real space)
-            auto1 = CatalogFFTPower(
-                data_positions1=ds1_positions, 
-                edges=edges, ells=(0), los=los, boxsize=boxsize, 
-                cellsize=cellsize, position_type='pos')
-            auto2 = CatalogFFTPower(
-                data_positions1=ds2_positions, 
-                edges=edges, ells=(0), los=los, boxsize=boxsize, 
-                cellsize=cellsize, position_type='pos')
-            auto3 = CatalogFFTPower(
-                data_positions1=ds3_positions, 
-                edges=edges, ells=(0), los=los, boxsize=boxsize, 
-                cellsize=cellsize, position_type='pos')
-            auto4 = CatalogFFTPower(
-                data_positions1=ds4_positions, 
-                edges=edges, ells=(0), los=los, boxsize=boxsize, 
-                cellsize=cellsize, position_type='pos')
-            auto5 = CatalogFFTPower(
-                data_positions1=ds5_positions, 
-                edges=edges, ells=(0), los=los, boxsize=boxsize, 
-                cellsize=cellsize, position_type='pos')
-            
-            # saves to file for later usage
-            pickle.dump(
-                cross1, open(os.path.join(power_folder_path, 
-                'sim{}_cross_ds1halo.pkl'.format(sim_num)), 'wb'))
-            pickle.dump(
-                cross2, open(os.path.join(power_folder_path, 
-                'sim{}_cross_ds2halo.pkl'.format(sim_num)), 'wb'))
-            pickle.dump(
-                cross3, open(os.path.join(power_folder_path, 
-                'sim{}_cross_ds3halo.pkl'.format(sim_num)), 'wb'))
-            pickle.dump(
-                cross4, open(os.path.join(power_folder_path, 
-                'sim{}_cross_ds4halo.pkl'.format(sim_num)), 'wb'))
-            pickle.dump(
-                cross5, open(os.path.join(power_folder_path, 
-                'sim{}_cross_ds5halo.pkl'.format(sim_num)), 'wb'))
-            
-            pickle.dump(
-                auto1, open(os.path.join(power_folder_path, 
-                'sim{}_auto_ds1.pkl'.format(sim_num)), 'wb'))
-            pickle.dump(
-                auto2, open(os.path.join(power_folder_path, 
-                'sim{}_auto_ds2.pkl'.format(sim_num)), 'wb'))
-            pickle.dump(
-                auto3, open(os.path.join(power_folder_path, 
-                'sim{}_auto_ds3.pkl'.format(sim_num)), 'wb'))
-            pickle.dump(
-                auto4, open(os.path.join(power_folder_path, 
-                'sim{}_auto_ds4.pkl'.format(sim_num)), 'wb'))
-            pickle.dump(
-                auto5, open(os.path.join(power_folder_path, 
-                'sim{}_auto_ds5.pkl'.format(sim_num)), 'wb'))
-            
         else:
-            print('Simulation {} failed.'.format(sim_num))
+            ds1_positions = None
+            ds5_positions = None
+        cross = CatalogFFTPower(
+            data_positions1=ds1_positions, data_positions2=ds5_positions,
+            edges=edges, ells=(0), los=los,
+            boxsize=boxsize, cellsize=cellsize, position_type='pos',
+            mpicomm=mpicomm, mpiroot=mpiroot)
+        pickle.dump(
+            cross, open(os.path.join(power_folder_path, 
+            'sim{}_cross_ds1-ds5.pkl'.format(sim_num), 'wb')))
+    print('Cross-Halo Power Spectra completed!')
+    
+    
+    
+    
     
     print('Script executed in {} seconds.'.format(time.time()-t0))
--- scripts/power_spectra.sh
+++ scripts/power_spectra.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --time=10:00:00
+#SBATCH --time=03:00:00
 #SBATCH --job-name=power_spectra
 #SBATCH --account=rrg-wperciva
 #SBATCH --ntasks=64
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=3G
-python get_power_spectra.py
+srun python get_power_spectra.py
